272c352314776b106b1b80214f6f435627e734eb
Nathan <nathan.ao.pang@gmail.com>
2022-04-23 16:38:53 -0700

    Skeleton code for database in place

--- MDBClient.py
+++ MDBClient.py
@@ -3,7 +3,29 @@ import GUI
 from pymongo import MongoClient
 
 #connects to the mongodb database and checks to see if its valid.
-client = pymongo.MongoClient("mongodb+srv://Aotae:<Aotae>@422notebook.j0r5l.mongodb.net/ARA_books?retryWrites=true&w=majority")
-db = client.test
 
+def insert_notes(notes):
+    # inserts one document into the library collection
+    # a document includes A users, Notes and the Book text of which the user is taking notes for
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client["ARA_books"]
+    collection = db["note-library"]
+    collection.insert_one(notes)
+    
+def insert_books(book):
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client["ARA_books"]
+    collection = db["library"]
+    collection.insert_one(notes)
+def update(id):
+    pass
+
+def delete(id):
+    pass
+
+def get(id):
+    pass
+
+def getlibrary():
+    pass
 
