912327b684e273ed92c17a6f58a037216ec1ba6c
Nathan <nathan.ao.pang@gmail.com>
2022-04-24 09:54:53 -0700

    left some comments on more db functions

--- MDBClient.py
+++ MDBClient.py
@@ -3,29 +3,43 @@ import GUI
 from pymongo import MongoClient
 
 #connects to the mongodb database and checks to see if its valid.
-
-def insert_notes(notes):
-    # inserts one document into the library collection
-    # a document includes A users, Notes and the Book text of which the user is taking notes for
+def connect():
     client = pymongo.MongoClient("mongodb://localhost:27017")
+    return client
+def insert_notes(notes):
+    # inserts one document into the 'note-library' collection
+    # a document includes a User's notes for a specified book
+    client = connect()
+    print("Hey we got here")
     db = client["ARA_books"]
     collection = db["note-library"]
     collection.insert_one(notes)
     
 def insert_books(book):
-    client = pymongo.MongoClient("mongodb://localhost:27017")
+    # inserts one document into the 'library' collection
+    # a document includes information on a book such as title and author as well as a cover image
+    client = connect()
     db = client["ARA_books"]
     collection = db["library"]
     collection.insert_one(book)
 def update(id):
+    # Put request that will update a users notes on a specified book by id
     pass
 
 def delete(id):
+    # delete request that will delete a users notes on a specified book by id
     pass
 
-def get(id):
+def get_notes(id):
+    # gets the notes from the 'note-library' collection according to bookname(id)
+    client = connect()
+    db = client["ARA_books"]
+    collection = db["note-library"]
+    return collection.find_one(id)
+def get_books(id):
+     # gets a book from the 'library' collection according to bookname(id)
     pass
-
 def getlibrary():
+     # returns all of the resources in the 'library' collection
     pass
 
