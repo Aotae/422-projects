ed7762fde7027a0c23e8b47dde3baa75eb46ee03
Nathan <nathan.ao.pang@gmail.com>
2022-04-23 22:19:51 -0700

    finished skeleton code for MDBClient as well as put in a placeholder get function, finished insert notes and connect

--- MDBClient.py
+++ MDBClient.py
@@ -7,8 +7,8 @@ def connect():
     client = pymongo.MongoClient("mongodb://localhost:27017")
     return client
 def insert_notes(notes):
-    # inserts one document into the library collection
-    # a document includes A users, Notes and the Book text of which the user is taking notes for
+    # inserts one document into the 'note-library' collection
+    # a document includes a User's notes for a specified book
     client = connect()
     print("Hey we got here")
     db = client["ARA_books"]
@@ -16,23 +16,30 @@ def insert_notes(notes):
     collection.insert_one(notes)
     
 def insert_books(book):
+    # inserts one document into the 'library' collection
+    # a document includes information on a book such as title and author as well as a cover image
     client = connect()
     db = client["ARA_books"]
     collection = db["library"]
     collection.insert_one(book)
 def update(id):
+    # Put request that will update a users notes on a specified book by id
     pass
 
 def delete(id):
+    # delete request that will delete a users notes on a specified book by id
     pass
 
 def get_notes(id):
+    # gets the notes from the 'note-library' collection according to bookname(id)
     client = connect()
     db = client["ARA_books"]
     collection = db["note-library"]
     return collection.find_one(id)
 def get_books(id):
+     # gets a book from the 'library' collection according to bookname(id)
     pass
 def getlibrary():
+     # returns all of the resources in the 'library' collection
     pass
 
